172a2518b323be61dafd32e4d88817d08746cc02
Richard Wei <xinranmsn@gmail.com>
2016-08-29 00:53:37 -0500

    Make compatible with 3.6 on Linux

--- LLVM.h
+++ LLVM.h
@@ -9,7 +9,7 @@
 #include <llvm-c/BitWriter.h>
 #include <llvm-c/Core.h>
 #include <llvm-c/Disassembler.h>
-#include <llvm-c/ErrorHandling.h>
+/* #include <llvm-c/ErrorHandling.h> */
 #include <llvm-c/ExecutionEngine.h>
 #include <llvm-c/Initialization.h>
 #include <llvm-c/IRReader.h>
@@ -17,7 +17,7 @@
 #include <llvm-c/LinkTimeOptimizer.h>
 #include <llvm-c/lto.h>
 #include <llvm-c/Object.h>
-#include <llvm-c/OrcBindings.h>
+/* #include <llvm-c/OrcBindings.h> */
 #include <llvm-c/Support.h>
 #include <llvm-c/Target.h>
 #include <llvm-c/TargetMachine.h>
@@ -25,6 +25,6 @@
 #include <llvm-c/Transforms/PassManagerBuilder.h>
 #include <llvm-c/Transforms/Scalar.h>
 #include <llvm-c/Transforms/Vectorize.h>
-#include <llvm-c/Types.h>
+/* #include <llvm-c/Types.h> */
 
 #endif // _LLVM_H_
